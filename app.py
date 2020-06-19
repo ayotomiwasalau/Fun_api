bbd357cf9ede0342a94dfcca0f97b6ad4e68cca5
tomiwasalau <ayotomiwasalau@gmail.com>
2020-06-19 16:07:01 -0700

    update to nltk use2

--- app.py
+++ app.py
@@ -234,7 +234,18 @@ def createapp(test_config=None):
 			riddle_answer = break_string_in_words(riddles.answer)
 
 
-			short_words = list(stopwords.words('english'))
+			short_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours',
+							'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers',
+							'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves',
+							'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are',
+							'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does',
+							'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until',
+							'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into',
+							'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down',
+							'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here',
+							'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',
+							'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so',
+							'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now']
 
 			def compare_words(response, riddle_answer, short_words): 
 				for w in set(response):
